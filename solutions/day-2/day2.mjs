307e90beb0ffac254eb174eaa5a79e4856f33935
Tony Montemuro <tonyamontemuro@gmail.com>
2023-12-01 23:40:43 -0600

    cleanup to day 2 solution

--- solutions/day-2/day2.mjs
+++ solutions/day-2/day2.mjs
@@ -1,48 +1,29 @@
 import readPuzzle from "../../read.mjs";
 
-const part1 = games => {
-    let total = 0;
-    const max = { red: 12, green: 13, blue: 14 };
-    games.forEach(game => {
-        let isPossible = true;
-        const components = game.split(":");
-        const id = components[0].split(" ")[1];
-        let sets = components[1].split(";");
-        for (let i = 0; i < sets.length; i++) {
-            sets[i] = sets[i].replace(" ", "");
-            sets[i].split(", ").forEach(set => {
-                const data = set.split(" ");
-                const num = data[0], color = data[1];
-                if (parseInt(num) > max[color]) isPossible = false;
-            });
-        }
-        if (isPossible) total += parseInt(id);
-    });
-    return total;
-};
-
-const part2 = games => {
-    let total = 0;
-    games.forEach(game => {
-        const minimum = { red: 0, green: 0, blue: 0 };
-        let sets = game.split(":")[1].split(";");
-        for (let i = 0; i < sets.length; i++) {
-            sets[i] = sets[i].replace(" ", "");
-            sets[i].split(", ").forEach(set => {
-                const data = set.split(" ");
-                const num = parseInt(data[0]), color = data[1];
-                if (num > minimum[color]) minimum[color] = num;
-            });
-        }
-        total += (minimum.red * minimum.green * minimum.blue);
-    });
-    return total;
-};
-
 const main = async () => {
     const data = await readPuzzle();
     const games = data.split("\n");
-    console.log(part1(games), part2(games));
+
+    let p1 = 0, p2 = 0;
+    games.forEach(game => {
+        const max = { red: 12, green: 13, blue: 14 }, minimum = { red: 0, green: 0, blue: 0 };
+        const components = game.split(":");
+        const id = components[0].split(" ")[1];
+        let sets = components[1].split(";");
+        let isPossible = true;
+        sets.forEach(set => {
+            set.replace(" ", "").split(", ").forEach(grab => {
+                const data = grab.split(" ");
+                const num = parseInt(data[0]), color = data[1];
+                if (num > max[color]) isPossible = false;
+                if (num > minimum[color]) minimum[color] = num;
+            });
+        });
+        if (isPossible) p1 += parseInt(id);
+        p2 += (minimum.red * minimum.green * minimum.blue);
+    });
+
+    console.log(p1, p2);
 };
 
 main();
