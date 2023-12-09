5587eab0720c9a28df2a3e99ae1f67322755517a
Tony Montemuro <tonyamontemuro@gmail.com>
2023-12-08 23:45:19 -0600

    day 9 solution

---
+++ solutions/day-9/day9.mjs
@@ -0,0 +1,46 @@
+import readPuzzle from "../../read.mjs";
+
+const p1 = differences => {
+    let total = 0;
+    for (let i = differences.length-1; i >= 0; i--) {
+        total += differences[i].at(-1);
+    }
+    return total;
+};
+
+const p2 = differences => {
+    let digit = differences.at(-1)[0];
+    const digits = [];
+    for (let i = differences.length-2; i >= 0; i--) {
+        digit = differences[i][0]-digit;
+        digits.push(digit);
+    }
+    return digits.at(-1);
+};
+
+const extrapolationSum = (sequences, extrapolator) => {
+    let total = 0;
+    sequences.forEach(sequence => {
+        sequence = sequence.split(" ").map(digit => parseInt(digit));
+        const differences = [sequence];
+        while (sequence.some(number => number !== 0)) {
+            const list = [];
+            for (let i = 1; i < sequence.length; i++) {
+                list.push(sequence[i]-sequence[i-1]);
+            }
+            sequence = list;
+            differences.push(list);
+        }
+        total += extrapolator(differences);
+    });
+
+    return total;
+};
+
+const main = async () => {
+    const data = await readPuzzle();
+    const sequences = data.split("\n");
+    console.log(extrapolationSum(sequences, p1), extrapolationSum(sequences, p2));
+};
+
+main();
