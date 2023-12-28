2bac3459505f7546dab86959a772decfd70410c1
Tony Montemuro <tonyamontemuro@gmail.com>
2023-12-28 01:01:36 -0600

    day 23 solution (warning: part 2 takes about 26 hours to run)

--- solutions/day-23/day23.mjs
+++ solutions/day-23/day23.mjs
@@ -1,85 +1,110 @@
 import readPuzzle from "../../read.mjs";
 
-const getNextMoves = (x, y, maze, visited) => {
-    const moves = [];
-    if (maze[y][x-1] !== "#" && !visited.includes(JSON.stringify([x-1, y]))) moves.push([-1, 0]);
-    if (maze[y-1][x] !== "#" && !visited.includes(JSON.stringify([x, y-1]))) moves.push([0, -1]);
-    if (maze[y][x+1] !== "#" && !visited.includes(JSON.stringify([x+1, y]))) moves.push([1, 0]);
-    if (maze[y+1][x] !== "#" && !visited.includes(JSON.stringify([x, y+1]))) moves.push([0, 1]);
-    return moves;
+const part1 = (x, y, seen, grid, end) => {
+    while (true) {
+        const coordinate = JSON.stringify([x, y]);
+        seen.push(coordinate);
+        const slopes = {
+            ">": [1, 0],
+            "<": [-1, 0],
+            "v": [0, 1],
+            "^": [0, -1]
+        };
+        const tile = grid[y][x];
+        if (x === end.x && y === end.y) {
+            return seen;
+        }
+        
+        if (Object.keys(slopes).includes(tile)) {
+            const x1 = x+slopes[tile][0];
+            const y1 = y+slopes[tile][1];
+            if (!seen.includes(JSON.stringify([x1, y1]))) {
+                x = x1, y = y1;
+            } else {
+                return seen;
+            }
+        } 
+        
+        else {
+            let surrounding = [], prev = seen.at(-2);
+            if (grid[y][x+1] !== "#" && prev !== JSON.stringify([x+1, y])) surrounding.push([x+1, y]);
+            if (grid[y][x-1] !== "#" && prev !== JSON.stringify([x-1, y])) surrounding.push([x-1, y]);
+            if (grid[y+1][x] !== "#" && prev !== JSON.stringify([x, y+1])) surrounding.push([x, y+1]);
+            if (grid[y-1][x] !== "#" && prev !== JSON.stringify([x, y-1])) surrounding.push([x, y-1]);
+            if (surrounding.length === 1) {
+                const [x1, y1] = surrounding[0];
+                if (!seen.includes(JSON.stringify([x1, y1]))) {
+                    x = x1, y = y1;
+                } else {
+                    return seen;
+                }
+            } else {
+                const paths = surrounding.map(next => {
+                    const [x1, y1] = next;
+                    return part1(x1, y1, [...seen], grid, end);
+                });
+                let max = [];
+                paths.forEach(path => {
+                    if (path.length > max.length && path.some(element => element === JSON.stringify([end.x, end.y]))) max = path;
+                });
+                return max;
+            }
+        }
+    }
 };
 
-const part1 = (x, y, maze, visited) => {
-    const coordinate = JSON.stringify([x, y]);
-    visited.push(coordinate);
-
-    // base case: we reached the end of the maze
-    if (y === maze.length-2) return visited;
-
-    // general case: we need to traverse the maze
-    const paths = [], tile = maze[y][x];
-    switch (tile) {
-        case "^":
-            const above = JSON.stringify([x, y-1]);
-            if (!visited.includes(above)) {
-                paths.push(part1(x, y-1, maze, visited));
+const part2 = (x, y, seen, grid, end) => {
+    while (true) {
+        const coordinate = JSON.stringify([x, y]);
+        seen.push(coordinate);
+        if (x === end.x && y === end.y) {
+            return seen;
+        }
+        
+        let surrounding = [], prev = seen.at(-2);
+        if (grid[y][x+1] !== "#" && prev !== JSON.stringify([x+1, y])) surrounding.push([x+1, y]);
+        if (grid[y][x-1] !== "#" && prev !== JSON.stringify([x-1, y])) surrounding.push([x-1, y]);
+        if (grid[y+1][x] !== "#" && prev !== JSON.stringify([x, y+1])) surrounding.push([x, y+1]);
+        if (grid[y-1][x] !== "#" && prev !== JSON.stringify([x, y-1])) surrounding.push([x, y-1]);
+        if (surrounding.length === 1) {
+            const [x1, y1] = surrounding[0];
+            if (!seen.includes(JSON.stringify([x1, y1]))) {
+                x = x1, y = y1;
             } else {
-                return visited;
+                return seen;
             }
-            break;
-        case "v":
-            const below = JSON.stringify([x, y+1]);
-            if (!visited.includes(below)) {
-                paths.push(part1(x, y+1, maze, visited));
-            } else {
-                return visited;
-            }
-            break;
-        case "<":
-            const left = JSON.stringify([x-1, y]);
-            if (!visited.includes(left)) {
-                paths.push(part1(x-1, y, maze, visited));
-            } else {
-                return visited;
-            }
-            break;
-        case ">":
-            const right = JSON.stringify([x+1, y]);
-            if (!visited.includes(right)) {
-                paths.push(part1(x+1, y, maze, visited));
-            } else {
-                return visited;
-            }
-            break;
-        default:
-            const next = getNextMoves(x, y, maze, visited);
-            next.forEach(m => {
-                paths.push(part1(x+m[0], y+m[1], maze, next.length > 1 ? [...visited] : visited));
+        } else {
+            const paths = surrounding.map(next => {
+                const [x1, y1] = next;
+                return part2(x1, y1, [...seen], grid, end);
             });
+            let max = [];
+            paths.forEach(path => {
+                if (path.length > max.length && path.some(element => element === JSON.stringify([end.x, end.y]))) max = path;
+            });
+            return max;
+        }
     }
-
-    // return path with longest length that reaches the goal
-    let max = [];
-    paths.forEach(path => {
-        if (path.length > max && path.some(element => JSON.parse(element)[1] === maze.length-2)) max = path;
-    });
-    return max;
 };
 
 const main = async () => {
     const data = await readPuzzle();
-    const maze = data.split("\n").map(row => row.split(""));
-    const padding = maze[0].map(i => "#");
-    maze.unshift(padding);
-    maze.push(padding);
-    let x0, y0 = 1;
-    maze[y0].forEach((tile, x) => {
+    const grid = data.split("\n").map(s => s.split(""));
+    const padding = grid[0].map(i => "#");
+    grid.unshift(padding);
+    grid.push(padding);
+    let y0 = 1, x0;
+    grid[y0].forEach((tile, x) => {
         if (tile === ".") x0 = x;
     });
-    
-    let visited = [];
-    const result = part1(x0, y0, maze, visited);
-    console.log(result, result.length);
-};
+    let y1 = grid.length-2, x1;
+    grid[y1].forEach((tile, x) => {
+        if (tile === ".") x1 = x;
+    });
+    const p1 = part1(x0, y0, [], grid, {x: x1, y: y1});
+    console.log(p1.length-1);
+    const p2 = part2(x0, y0, [], grid, {x: x1, y: y1});
+    console.log(p2.length-1);
+}
 
 main();
